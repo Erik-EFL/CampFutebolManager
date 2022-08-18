2edbd312cec70bcf22409239dd93ca72ad537712
erik lima <erik.lima.f@outlook.com>
2022-08-18 19:27:59 -0300

    feat(app/backend): Foi criada uma tabela para match
    Esta tabela ira armazenar confrontos

---
+++ app/backend/src/database/models/match.ts
@@ -0,0 +1,46 @@
+import { INTEGER, BOOLEAN, Model } from 'sequelize';
+import db from '.';
+
+export default class Match extends Model {
+  declare id: number;
+  declare homeTeam: number;
+  declare homeTeamGoals: number;
+  declare awayTeam: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Match.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  tableName: 'matches',
+  timestamps: false,
+});
