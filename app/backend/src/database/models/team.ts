6527f01393bee8551bb0479f1a1f87dbc1d6f62d
erik lima <erik.lima.f@outlook.com>
2022-08-18 19:27:06 -0300

    feat(app/backend): :sparkles: Criando e populando tabela
    Foi criada uma tabela para teams
    
    A tabela Teams ira armazenar informações sobre os times pertencentes ao campeonato jogado

---
+++ app/backend/src/database/models/team.ts
@@ -0,0 +1,30 @@
+import { INTEGER, STRING, Model } from 'sequelize';
+import db from '.';
+
+export default class Team extends Model {
+  declare id: number;
+  declare homeTeam: number;
+  declare homeTeamGoals: number;
+  declare awayTeam: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Team.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  tableName: 'teams',
+  timestamps: false,
+});
