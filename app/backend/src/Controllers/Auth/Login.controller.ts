412d07692ef91b175342559d6deaf5ee406892d4
erik lima <erik.lima.f@outlook.com>
2022-08-23 18:53:38 -0300

    refactor(app/backend): :recycle: Refattorando codigo para poder fazer validações direto nas rotas
    Criando validação de toquem para ser feita direta na rota

--- app/backend/src/Controllers/Auth/Login.controller.ts
+++ app/backend/src/Controllers/Auth/Login.controller.ts
@@ -1,12 +1,11 @@
-import { Request, Response } from 'express';
-import CustomError from '../../middlewares/errors/Custom.error';
+import { Request, Response, NextFunction } from 'express';
+import JwtAuth from '../../services/Auth/Jwt.services';
+import { validateBody, validateHeader } from '../../middlewares/validations/BodyLogin.validate';
 import LoginAuth from '../../services/Auth/Login.services';
 
 export default class LoginController {
   login = async (req: Request, res: Response) => {
-    const { email, password } = req.body;
-
-    if (!email || !password) return res.status(400).json({ message: 'All fields must be filled' });
+    const { email, password } = validateBody(req.body);
 
     const token = await LoginAuth.login(email, password);
 
@@ -14,12 +13,20 @@ export default class LoginController {
   };
 
   validate = async (req: Request, res: Response) => {
-    const { authorization: token } = req.headers;
-
-    if (!token) throw new CustomError(401, 'You must pass a token');
+    const { authorization } = req.headers;
+    const token = validateHeader(authorization as string);
 
     const role = await LoginAuth.validateLogin(token);
 
     res.status(200).json({ role });
+  };
+
+  tokenValidation = async (req: Request, _res: Response, next: NextFunction) => {
+    const { authorization } = req.headers;
+    const token = validateHeader(authorization as string);
+
+    JwtAuth.tokenVerify(token);
+
+    next();
   };
 }
