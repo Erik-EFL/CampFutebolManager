5a93026340457ec65883b779ac263e20086b4d85
erik lima <erik.lima.f@outlook.com>
2022-08-26 14:34:09 -0300

    refactor(app/backend): :sparkles: Criando metodos na class CustonError
    Adicionando metodos para disparar error
    
    Ao adicionar os metodos que disparam os erros eu torno o codigo mais legivel, mais limpo e deixo a função de disparar error somente com quem deve fazer

--- app/backend/src/middlewares/errors/Custom.error.ts
+++ app/backend/src/middlewares/errors/Custom.error.ts
@@ -5,4 +5,24 @@ export default class CustomError extends Error {
     super(message);
     this.status = status;
   }
+
+  static notFound(message: string) {
+    throw new CustomError(404, message);
+  }
+
+  static badRequest(message: string) {
+    throw new CustomError(400, message);
+  }
+
+  static unauthorized(message: string) {
+    throw new CustomError(401, message);
+  }
+
+  static conflict(message: string) {
+    throw new CustomError(409, message);
+  }
+
+  static serverError(message: string) {
+    throw new CustomError(500, message);
+  }
 }
