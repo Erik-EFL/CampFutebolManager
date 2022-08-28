cd8276e6adbb17c8f62e02bc3985f110ac27a307
erik lima <erik.lima.f@outlook.com>
2022-08-28 02:14:24 -0300

    feat(app/backend): :sparkles: Adicionando novas rotas a leader board

--- app/backend/src/routes/leaderBoard.routes.ts
+++ app/backend/src/routes/leaderBoard.routes.ts
@@ -4,6 +4,8 @@ import LeaderBoardController from '../Controllers/leaderBoard.controller';
 const ranking = new LeaderBoardController();
 const leaderBoard = Router();
 
-leaderBoard.get('/home', ranking.get.teamLeaderBoardHome);
+leaderBoard.get('/home', (req, res) => ranking.get.teamLeaderBoardHome(req, res));
+leaderBoard.get('/away', (req, res) => ranking.get.teamLeaderBoardAway(req, res));
+leaderBoard.get('/', (req, res) => ranking.get.teamLeaderBoard(req, res));
 
 export default leaderBoard;
