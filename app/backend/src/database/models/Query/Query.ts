1542a1afc129765d2e0b3783962ef6a43a396795
erik lima <erik.lima.f@outlook.com>
2022-08-28 02:12:36 -0300

    feat(app/backend): :sparkles: Criando model
    Criando Query e model para leader board
    
    Criadas querys para consular learderBoard Home e away

--- app/backend/src/database/models/Query/Query.ts
+++ app/backend/src/database/models/Query/Query.ts
@@ -1,4 +1,4 @@
-const leaderBoard = `
+const leaderBoardHome = `
 SELECT
 tm.team_name AS name,
     SUM(IF(tfcMtc.home_team_goals > tfcMtc.away_team_goals, 3,
@@ -26,4 +26,64 @@ ON tm.id = tfcMtc.home_team
     goalsOwn DESC;
 `;
 
-export default leaderBoard;
+const leaderBoardAway = `
+SELECT
+tm.team_name AS name,
+    SUM(IF(tfcMtc.away_team_goals > tfcMtc.home_team_goals, 3,
+IF(tfcMtc.away_team_goals < tfcMtc.home_team_goals, 0, 1))) AS totalPoints,
+    COUNT(tm.team_name) AS totalGames,
+    SUM(IF(tfcMtc.away_team_goals > tfcMtc.home_team_goals, 1, 0)) AS totalVictories,
+    SUM(IF(tfcMtc.away_team_goals = tfcMtc.home_team_goals, 1, 0)) AS totalDraws,
+    SUM(IF(tfcMtc.away_team_goals < tfcMtc.home_team_goals, 1, 0)) AS totalLosses,
+    SUM(tfcMtc.away_team_goals) AS goalsFavor,
+    SUM(tfcMtc.home_team_goals) AS goalsOwn,
+    SUM(tfcMtc.away_team_goals) - SUM(tfcMtc.home_team_goals) AS goalsBalance,
+    TRIM(ROUND(SUM(IF(tfcMtc.away_team_goals > tfcMtc.home_team_goals, 3,
+IF(tfcMtc.away_team_goals < tfcMtc.home_team_goals, 0,1)))
+      / (COUNT(tm.team_name) * 3) * 100, 2)) + 0 AS efficiency
+FROM TRYBE_FUTEBOL_CLUBE.teams AS tm
+INNER JOIN TRYBE_FUTEBOL_CLUBE.matches AS tfcMtc
+ON tm.id = tfcMtc.away_team
+  GROUP BY tm.team_name, tfcMtc.in_progress
+  HAVING tfcMtc.in_progress = 0
+  ORDER BY
+    totalPoints DESC ,
+    totalVictories DESC ,
+    goalsBalance DESC ,
+    goalsFavor DESC ,
+    goalsOwn DESC;
+`;
+
+const leaderBoard = `
+SELECT
+tm.team_name AS name,
+    SUM(IF(tfcMtc.away_team_goals > tfcMtc.home_team_goals, 3,
+IF(tfcMtc.away_team_goals < tfcMtc.home_team_goals, 0, 1))) AS totalPoints,
+    COUNT(tm.team_name) AS totalGames,
+    SUM(IF(tfcMtc.away_team_goals > tfcMtc.home_team_goals, 1, 0)) AS totalVictories,
+    SUM(IF(tfcMtc.away_team_goals = tfcMtc.home_team_goals, 1, 0)) AS totalDraws,
+    SUM(IF(tfcMtc.away_team_goals < tfcMtc.home_team_goals, 1, 0)) AS totalLosses,
+    SUM(tfcMtc.away_team_goals) AS goalsFavor,
+    SUM(tfcMtc.home_team_goals) AS goalsOwn,
+    SUM(tfcMtc.away_team_goals) - SUM(tfcMtc.home_team_goals) AS goalsBalance,
+    TRIM(ROUND(SUM(IF(tfcMtc.away_team_goals > tfcMtc.home_team_goals, 3,
+IF(tfcMtc.away_team_goals < tfcMtc.home_team_goals, 0,1)))
+      / (COUNT(tm.team_name) * 3) * 100, 2)) + 0 AS efficiency
+FROM TRYBE_FUTEBOL_CLUBE.teams AS tm
+INNER JOIN TRYBE_FUTEBOL_CLUBE.matches AS tfcMtc
+ON tm.id = tfcMtc.home_team
+  GROUP BY tm.team_name, tfcMtc.in_progress
+  HAVING tfcMtc.in_progress = 0
+  ORDER BY
+    totalPoints DESC,
+    totalVictories DESC,
+    goalsBalance DESC,
+    goalsFavor DESC,
+    goalsOwn DESC;
+`;
+
+export {
+  leaderBoard,
+  leaderBoardHome,
+  leaderBoardAway,
+};
--- app/backend/src/database/models/leaderBoard.ts
+++ app/backend/src/database/models/leaderBoard.ts
@@ -1,10 +1,22 @@
 import db from '.';
-import leaderBoard from './Query/Query';
+import { leaderBoard, leaderBoardAway, leaderBoardHome } from './Query/Query';
 
 export default class LeaderBoardQuery {
+  static async teamLeaderBoardHome() {
+    const dbQuery = leaderBoardHome;
+    const [rows] = await db.query(dbQuery);
+    return rows;
+  }
+
+  static async teamLeaderBoardAway() {
+    const dbQuery = leaderBoardAway;
+    const [rows] = await db.query(dbQuery);
+    return rows;
+  }
+
   static async teamLeaderBoard() {
     const dbQuery = leaderBoard;
-    const [rows] = await db.query(dbQuery);
+    const rows = await db.query(dbQuery);
     return rows;
   }
 }
