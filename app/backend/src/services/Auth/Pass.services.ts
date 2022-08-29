95de81781a6f1b3527881505b108da89fc602375
erik lima <erik.lima.f@outlook.com>
2022-08-28 22:58:41 -0300

    refactor(app/backend): :art: Removendo estrutura
    Retirando declaração de contructor

--- app/backend/src/services/Auth/Pass.services.ts
+++ app/backend/src/services/Auth/Pass.services.ts
@@ -2,13 +2,6 @@ import * as bcrypt from 'bcryptjs';
 import CustomError from '../../middlewares/errors/Custom.error';
 
 export default class PassWordAuth extends CustomError {
-  status: number;
-  constructor(status: number, message: string) {
-    super(status, message);
-    this.status = status;
-    this.message = message;
-  }
-
   static comparePassword(dbPassword: string, password: string): boolean {
     const hashPassword = bcrypt.compareSync(password, dbPassword);
     if (!hashPassword) this.unauthorized('Incorrect email or password');
